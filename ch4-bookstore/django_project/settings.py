d3a05cceba4a6a9f862d791970b8127dbd843f62
Natalia Oliveira <nataliaferreiraoliveira1994@gmail.com>
2023-01-30 08:18:12 -0300

    add chapter 10 - password

--- ch4-bookstore/django_project/settings.py
+++ ch4-bookstore/django_project/settings.py
@@ -162,6 +162,17 @@ AUTHENTICATION_BACKENDS = (
 )
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+# EMAIL_HOST = "smtp.gmail.com"
+
+# EMAIL_USE_TLS = True
+
+# EMAIL_PORT = 587
+
+# EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+
+# EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = "admin@djangobookstore.com"
 
