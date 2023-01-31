2dab1d45a440eb72a9f6db9e0303f02ea9f6ced1
Natalia Oliveira <nataliaferreiraoliveira1994@gmail.com>
2023-01-31 08:48:15 -0300

    add chapter 11 - until individual book page

---
+++ ch4-bookstore/books/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import DetailView, ListView
+
+from .models import Book
+
+
+class BookListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/book_list.html"
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "books/book_detail.html"
--- ch4-bookstore/django_project/settings.py
+++ ch4-bookstore/django_project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     # Local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
+    "books.apps.BooksConfig",
 ]
 
 MIDDLEWARE = [
