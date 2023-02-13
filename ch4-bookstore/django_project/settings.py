6a0bc148320eb4c593b45b99ed238ed473fc97a2
Natalia Oliveira <nataliaferreiraoliveira1994@gmail.com>
2023-02-13 07:22:11 -0300

    add chapter 13

--- ch4-bookstore/django_project/settings.py
+++ ch4-bookstore/django_project/settings.py
@@ -188,6 +188,11 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
+# Media files
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- ch4-bookstore/django_project/urls.py
+++ ch4-bookstore/django_project/urls.py
@@ -1,4 +1,5 @@
 """django_project URL Configuration
+ck
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -24,4 +27,4 @@ urlpatterns = [
     # Local apps
     path("", include("pages.urls")),
     path("books/", include("books.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
