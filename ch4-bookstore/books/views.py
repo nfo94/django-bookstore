69090e7949c66cb5e6d9e5b2e44adca0152e4fb0
Natalia Oliveira <nataliaferreiraoliveira1994@gmail.com>
2023-02-15 08:50:55 -0300

    add chapter 15 - until q objects

--- ch4-bookstore/books/views.py
+++ ch4-bookstore/books/views.py
@@ -17,3 +17,10 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "books.special_status"
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+    queryset = Book.objects.filter(title__icontains="second")
