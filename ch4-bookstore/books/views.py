208e7e9090e44c284c4025da32d423a12fd0f440
Natalia Oliveira <nataliaferreiraoliveira1994@gmail.com>
2023-02-20 08:06:19 -0300

    add chapter 15

--- ch4-bookstore/books/views.py
+++ ch4-bookstore/books/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
 from django.views.generic import DetailView, ListView
 
 from .models import Book
@@ -23,4 +24,9 @@ class SearchResultsListView(ListView):
     model = Book
     context_object_name = "book_list"
     template_name = "books/search_results.html"
-    queryset = Book.objects.filter(title__icontains="second")
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
--- ch4-bookstore/django_project/settings.py
+++ ch4-bookstore/django_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
